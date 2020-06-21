45e2b896df6c3d01938daede88f1e7a113e48457
shoter <lubiebardzozelki@gmail.com>
2020-06-21 23:06:27 +0200

    Added OttdDate Tests

---
+++ OpenTTDAdminPort.Tests/OttdDateShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OpenTTDAdminPort.Tests
+{
+    public class OttdDateShould
+    {
+        [InlineData(706891, 1935, 4, 28)]
+        [Theory]
+        public void ConstructProperDate_FromTicks(uint tick, uint year, byte month, byte day)
+        {
+            OttdDate date = new OttdDate(tick);
+
+            Assert.Equal(year, date.Year);
+            Assert.Equal(month, date.Month);
+            Assert.Equal(day, date.Day);
+        }
+    }
+}
