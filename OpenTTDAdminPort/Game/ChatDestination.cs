b90514a1283760777254090f41b34a7e7747c44d
shoter <lubiebardzozelki@gmail.com>
2020-06-23 01:31:32 +0200

    Moved game related things to Game namespace

---
+++ OpenTTDAdminPort/Game/ChatDestination.cs
@@ -0,0 +1,18 @@
+﻿namespace OpenTTDAdminPort.Game
+{
+    public enum ChatDestination
+    {
+        /// <summary>
+        /// Send message/notice to all clients (All)
+        /// </summary>
+        DESTTYPE_BROADCAST,
+        /// <summary>
+        ///  Send message/notice to everyone playing the same company (Team)
+        /// </summary>
+        DESTTYPE_TEAM,
+        /// <summary>
+        ///  Send message/not   ice to only a certain client (Private)
+        /// </summary>
+        DESTTYPE_CLIENT,
+    }
+}
---
+++ OpenTTDAdminPort/Game/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace OpenTTDAdminPort.Game
+{
+    public class Player
+    {
+        public uint ClientId { get; }
+        public string Name { get; set; }
+
+        public Player(uint clientId) => this.ClientId = clientId;
+
+        public Player(uint clientId, string name)
+            : this(clientId) => this.Name = name;
+
+    }
+}
