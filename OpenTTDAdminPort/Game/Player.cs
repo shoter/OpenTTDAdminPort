71c3880a7100dcbda5ec78ebcda1da9109a9f222
shoter <lubiebardzozelki@gmail.com>
2020-06-23 01:32:26 +0200

    Player constructor refactor

--- OpenTTDAdminPort/Game/Player.cs
+++ OpenTTDAdminPort/Game/Player.cs
@@ -5,10 +5,11 @@
         public uint ClientId { get; }
         public string Name { get; set; }
 
-        public Player(uint clientId) => this.ClientId = clientId;
-
         public Player(uint clientId, string name)
-            : this(clientId) => this.Name = name;
+        {
+            this.ClientId = clientId;
+            this.Name = name;
+        }
 
     }
 }
