0f91acf777babeda18e9e004a27342b4f7c095cc
shoter <lubiebardzozelki@gmail.com>
2020-06-25 12:04:35 +0200

    Unit tests for ClassTypeMatcherShould

---
+++ OpenTTDAdminPort.Tests/Assemblies/ClassTypeMatcherShould.cs
@@ -0,0 +1,30 @@
+﻿using OpenTTDAdminPort.Assemblies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OpenTTDAdminPort.Tests.Assemblies
+{
+    public class ClassTypeMatcherShould
+    {
+        ClassTypeMatcher matcher = new ClassTypeMatcher();
+        interface someInt { }
+
+        class someClass { }
+
+        struct someStruct { }
+
+        [Fact]
+        public void NotMatchInterface() => Assert.False(matcher.IsMatching(typeof(someInt)));
+
+        [Fact]
+        public void MatchClass() => Assert.True(matcher.IsMatching(typeof(someClass)));
+        [Fact]
+        public void NotMatchStruct() => Assert.False(matcher.IsMatching(typeof(someStruct)));
+
+
+    }
+}
