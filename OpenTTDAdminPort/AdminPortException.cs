66fb8939cdd9bdfaf9de01aa6b04d77de0dbf8d4
shoter <lubiebardzozelki@gmail.com>
2020-06-26 03:25:53 +0200

    Removed unused constructors in  Admin Port Excpetion

--- OpenTTDAdminPort/AdminPortException.cs
+++ OpenTTDAdminPort/AdminPortException.cs
@@ -5,19 +5,7 @@ namespace OpenTTDAdminPort
 {
     public class AdminPortException : Exception
     {
-        public AdminPortException()
-        {
-        }
-
         public AdminPortException(string message) : base(message)
-        {
-        }
-
-        public AdminPortException(string message, Exception innerException) : base(message, innerException)
-        {
-        }
-
-        protected AdminPortException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
