37c184dc2aa4eb752ac7c6436eefffb657e3de50
shoter <lubiebardzozelki@gmail.com>
2020-06-28 04:06:12 +0200

    Added more test for AdminPortErroredOutStateShould

--- OpenTTDAdminPort.Tests/Networking/AdminPortErroredOutStateShould.cs
+++ OpenTTDAdminPort.Tests/Networking/AdminPortErroredOutStateShould.cs
@@ -4,6 +4,7 @@ using OpenTTDAdminPort.States;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -14,12 +15,37 @@ namespace OpenTTDAdminPort.Tests.Networking
     {
         AdminPortErroredOutState state = new AdminPortErroredOutState();
 
+        public AdminPortErroredOutStateShould()
+        {
+            context.State = AdminConnectionState.ErroredOut;
+        }
+
         [Fact]
         public void ThrowException_ForApiUsedByUser()
         {
             Assert.Throws<AdminPortException>(() => state.SendMessage(Mock.Of<IAdminMessage>(), context));
             Assert.ThrowsAsync<AdminPortException>(async () => await state.Connect(context));
             Assert.ThrowsAsync<AdminPortException>(async () => await state.Disconnect(context));
+        }
+
+        [Fact]
+        public void ClearMessageQueue_WhenStateStarts()
+        {
+            context.MessagesToSend.Enqueue(Mock.Of<IAdminMessage>());
+            state.OnStateStart(context);
+            Assert.Empty(context.MessagesToSend);
+        }
+
+        [Fact]
+        public void DoNothing_OnMethodsExecutedOnStateChangeOrReceivedMessage()
+        {
+            state.OnStateStart(context);
+            state.OnMessageReceived(Mock.Of<IAdminMessage>(), context);
+            state.OnStateEnd(context);
+
+            Assert.Empty(context.MessagesToSend);
+            tcpClientMock.Verify(x => x.SendMessage(It.IsAny<IAdminMessage>()), Times.Never);
+            Assert.Equal(AdminConnectionState.ErroredOut, context.State);
         }
     }
 }
