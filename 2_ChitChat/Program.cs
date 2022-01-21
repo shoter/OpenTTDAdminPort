e4c94bc22d101043333ee34cadf4b22e9851c8e1
Damian Laczak <shoter@users.noreply.github.com>
2022-01-21 22:24:08 +0100

    feat: add chit-chat

---
+++ 2_ChitChat/Program.cs
@@ -0,0 +1,45 @@
+﻿using OpenTTDAdminPort;
+using OpenTTDAdminPort.Events;
+using OpenTTDAdminPort.Game;
+using OpenTTDAdminPort.Messages;
+using System;
+using System.Collections.Concurrent;
+using System.Dynamic;
+
+namespace _2_ChitChat
+{
+    class Program
+    {
+        public static ConcurrentQueue<IAdminEvent> EventQueue { get; } = new ConcurrentQueue<IAdminEvent>(); 
+        static async void Main(string[] args)
+        {
+            var client = new AdminPortClient(new ServerInfo(
+            "127.0.0.1", 3982, "admin_pass"));
+
+            // This will be running on different thread - it is important to use multi-thread safe constructs that will enable sharing data between client and our thread.
+            client.EventReceived += (_, ev) => EventQueue.Enqueue(ev);
+            await client.Connect();
+
+            while(true)
+            {
+                Console.WriteLine("Write chat message: (blank message to receive new messages from server)");
+                string line = Console.ReadLine();
+
+                while(EventQueue.TryDequeue(out IAdminEvent ev))
+                {
+                    if (ev is AdminChatMessageEvent chatEvent)
+                    {
+                        Console.WriteLine($"{chatEvent.Player.Name}: {chatEvent.Message}");
+                    }
+                }
+
+                if(string.IsNullOrWhiteSpace(line))
+                {
+                    continue;
+                }
+
+                client.SendMessage(new AdminChatMessage(NetworkAction.NETWORK_ACTION_CHAT, ChatDestination.DESTTYPE_BROADCAST, 0, line));
+            }
+        }
+    }
+}
