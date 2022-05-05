7bc2279dcff7a0a83a5a2eb88249e8da58683bfa
Damian Laczak <shoter@users.noreply.github.com>
2022-05-06 00:54:56 +0200

    feat: PingPongTest

--- OpenTTDAdminPort.Tests/AdminPortClientShould.cs
+++ OpenTTDAdminPort.Tests/AdminPortClientShould.cs
@@ -1,99 +1,94 @@
-﻿//using Moq;
-//using OpenTTDAdminPort.Events;
-//using OpenTTDAdminPort.Messages;
-//using OpenTTDAdminPort.Networking;
-//using OpenTTDAdminPort.States;
-//using OpenTTDAdminPort.Tests.Messages;
-//using OpenTTDAdminPort.Tests.Networking;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Xunit;
+﻿using Moq;
 
-//namespace OpenTTDAdminPort.Tests
-//{
-//    public class AdminPortClientShould
-//    {
-//        Mock<IAdminPortTcpClient> tcpClientMock = new Mock<IAdminPortTcpClient>();
-//        Mock<IAdminEventFactory> messageProcessorMock = new Mock<IAdminEventFactory>();
+using OpenTTDAdminPort.Events;
+using OpenTTDAdminPort.Messages;
+using OpenTTDAdminPort.Tests.Messages;
 
-//        IAdminPortClient client;
-//        public AdminPortClientShould()
-//        {
-//            tcpClientMock.CallBase = true;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
-//            client = new AdminPortClient(tcpClientMock.Object, messageProcessorMock.Object, AdminPortClientSettings.Default,
-//                new ServerInfo("127.0.0.1", 123, "LubiePlacki"));
-//        }
+using Xunit;
 
-//        [Fact]
-//        public void NotConnect_AfterObjectIsConstructed()
-//        {
-//            tcpClientMock.Verify(x => x.Start(It.IsAny<string>(), It.IsAny<int>()), Times.Never);
-//        }
+namespace OpenTTDAdminPort.Tests
+{
+    public class AdminPortClientShould
+    {
+        //IAdminPortClient client;
+        //public AdminPortClientShould()
+        //{
+        //    tcpClientMock.CallBase = true;
 
-//        [Fact]
-//        public void StartConnectToServer_WhenConnectWasExecutedForFirstTime()
-//        {
-//            client.Connect();
-//            tcpClientMock.Verify(x => x.Start(client.ServerInfo.ServerIp, client.ServerInfo.ServerPort));
-//            Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
-//            }
+        //    client = new AdminPortClient(AdminPortClientSettings.Default,
+        //        new ServerInfo("127.0.0.1", 123, "LubiePlacki"));
+        //}
 
-//        [Fact]
-//        public async Task ErrorOut_AfterConnecting_WhenStateWillNotTurnIntoConnected_After10Seconds()
-//        {
-//            await Assert.ThrowsAsync<AdminPortException>(async () => await client.Connect());
-//            Assert.Equal(AdminConnectionState.ErroredOut, client.ConnectionState);
-//        }
+        //[Fact]
+        //public void NotConnect_AfterObjectIsConstructed()
+        //{
+        //    tcpClientMock.Verify(x => x.Start(It.IsAny<string>(), It.IsAny<int>()), Times.Never);
+        //}
 
-//        [Fact]
-//        public async Task ConnectToServer()
-//        {
-//            Task connectTask = client.Connect();
-//            Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
-//            // tcp client should receive connect message by now.
-//            tcpClientMock.Verify(x => x.SendMessage(It.Is<IAdminMessage>(msg => msg is AdminJoinMessage)));
-//            // we can send welcome messages to our dear client.
-//            tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerProtocolMessage(0, new Dictionary<Game.AdminUpdateType, Game.UpdateFrequency>()));
-//            tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerWelcomeMessageFixture().Build());
-//            // client should be now connected.
-//            Assert.Equal(AdminConnectionState.Connected, client.ConnectionState);
-//            // it will throw exception if this will not connect. This is basicly assertion of this test.
-//            await connectTask;
-//        }
+        //[Fact]
+        //public void StartConnectToServer_WhenConnectWasExecutedForFirstTime()
+        //{
+        //    client.Connect();
+        //    tcpClientMock.Verify(x => x.Start(client.ServerInfo.ServerIp, client.ServerInfo.ServerPort));
+        //    Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
+        //}
 
-//        [Fact]
-//        public async Task TurnIntoConnecting_WhenTcpClientErrorsOut()
-//        {
-//            await Connect();
-//            tcpClientMock.Raise(x => x.Errored += null, this, new Exception("Boom!"));
-//            Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
-//        }
+        //[Fact]
+        //public async Task ErrorOut_AfterConnecting_WhenStateWillNotTurnIntoConnected_After10Seconds()
+        //{
+        //    await Assert.ThrowsAsync<AdminPortException>(async () => await client.Connect());
+        //    Assert.Equal(AdminConnectionState.ErroredOut, client.ConnectionState);
+        //}
 
-//        [Fact]
-//        public async Task BeAbleToSendMessage_AfterConnecting()
-//        {
-//            await Connect();
-//            var msg = Mock.Of<IAdminMessage>();
-//            client.SendMessage(msg);
-//            tcpClientMock.Verify(x => x.SendMessage(msg), Times.Once);
-//        }
+        //[Fact]
+        //public async Task ConnectToServer()
+        //{
+        //    Task connectTask = client.Connect();
+        //    Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
+        //    // tcp client should receive connect message by now.
+        //    tcpClientMock.Verify(x => x.SendMessage(It.Is<IAdminMessage>(msg => msg is AdminJoinMessage)));
+        //    // we can send welcome messages to our dear client.
+        //    tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerProtocolMessage(0, new Dictionary<Game.AdminUpdateType, Game.UpdateFrequency>()));
+        //    tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerWelcomeMessageFixture().Build());
+        //    // client should be now connected.
+        //    Assert.Equal(AdminConnectionState.Connected, client.ConnectionState);
+        //    // it will throw exception if this will not connect. This is basicly assertion of this test.
+        //    await connectTask;
+        //}
 
-//        private async Task Connect()
-//        {
-//            Task connectTask = client.Connect();
-//            Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
-//            // tcp client should receive connect message by now.
-//            tcpClientMock.Verify(x => x.SendMessage(It.Is<IAdminMessage>(msg => msg is AdminJoinMessage)));
-//            // we can send welcome messages to our dear client.
-//            tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerProtocolMessage(0, new Dictionary<Game.AdminUpdateType, Game.UpdateFrequency>()));
-//            tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerWelcomeMessageFixture().Build());
-//            // client should be now connected.
-//            Assert.Equal(AdminConnectionState.Connected, client.ConnectionState);
-//            await connectTask;
-//        }
-//    }
-//}
+        //[Fact]
+        //public async Task TurnIntoConnecting_WhenTcpClientErrorsOut()
+        //{
+        //    await Connect();
+        //    tcpClientMock.Raise(x => x.Errored += null, this, new Exception("Boom!"));
+        //    Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
+        //}
+
+        //[Fact]
+        //public async Task BeAbleToSendMessage_AfterConnecting()
+        //{
+        //    await Connect();
+        //    var msg = Mock.Of<IAdminMessage>();
+        //    client.SendMessage(msg);
+        //    tcpClientMock.Verify(x => x.SendMessage(msg), Times.Once);
+        //}
+
+        //private async Task Connect()
+        //{
+        //    Task connectTask = client.Connect();
+        //    Assert.Equal(AdminConnectionState.Connecting, client.ConnectionState);
+        //    // tcp client should receive connect message by now.
+        //    tcpClientMock.Verify(x => x.SendMessage(It.Is<IAdminMessage>(msg => msg is AdminJoinMessage)));
+        //    // we can send welcome messages to our dear client.
+        //    tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerProtocolMessage(0, new Dictionary<Game.AdminUpdateType, Game.UpdateFrequency>()));
+        //    tcpClientMock.Raise(x => x.MessageReceived += null, this, new AdminServerWelcomeMessageFixture().Build());
+        //    // client should be now connected.
+        //    Assert.Equal(AdminConnectionState.Connected, client.ConnectionState);
+        //    await connectTask;
+        //}
+    }
+}
--- OpenTTDAdminPort.Tests/Dockerized/AdminPortClientTests.cs
+++ OpenTTDAdminPort.Tests/Dockerized/AdminPortClientTests.cs
@@ -1,128 +1,138 @@
-﻿//using Divergic.Logging.Xunit;
+﻿using Divergic.Logging.Xunit;
 
-//using Microsoft.Extensions.Logging;
-//using Microsoft.Extensions.Logging.Debug;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Debug;
 
-//using OpenTTDAdminPort.Common;
-//using OpenTTDAdminPort.Events;
-//using OpenTTDAdminPort.Logging;
-//using OpenTTDAdminPort.Messages;
-//using OpenTTDAdminPort.Tests.Dockerized.Containers;
+using OpenTTDAdminPort.Common;
+using OpenTTDAdminPort.Events;
+using OpenTTDAdminPort.Logging;
+using OpenTTDAdminPort.Messages;
+using OpenTTDAdminPort.Tests.Dockerized.Containers;
+using OpenTTDAdminPort.Tests.Logging;
 
-//using System;
-//using System.Threading.Tasks;
+using System;
+using System.Threading.Tasks;
 
-//using Xunit;
-//using Xunit.Abstractions;
+using Xunit;
+using Xunit.Abstractions;
 
-//namespace OpenTTDAdminPort.Tests.Dockerized
-//{
-//    public class AdminPortClientTests : IDisposable
-//    {
-//        private readonly ILoggerFactory loggerFactory;
-//        private readonly OpenttdServerContainer server = new OpenttdServerContainer(DockerClientProvider.Instance);
-//        private bool disposedValue;
-//        private ILogger logger;
+namespace OpenTTDAdminPort.Tests.Dockerized
+{
+    public class AdminPortClientTests : IDisposable
+    {
+        private readonly ILoggerFactory loggerFactory;
+        private readonly OpenttdServerContainer server = new OpenttdServerContainer(DockerClientProvider.Instance);
+        private bool disposedValue;
+        private ILogger logger;
+        ITestOutputHelper output;
 
-//        public AdminPortClientTests(ITestOutputHelper output)
-//        {
-//            loggerFactory = LogFactory.Create(output);
-//            loggerFactory.AddProvider(new DebugLoggerProvider());
-//            logger = loggerFactory.CreateLogger<AdminPortClientTests>();
-//        }
+        public AdminPortClientTests(ITestOutputHelper output)
+        {
+            this.output = output;
+            loggerFactory = LogFactory.Create(output);
+            loggerFactory.AddProvider(new DebugLoggerProvider());
+            logger = loggerFactory.CreateLogger<AdminPortClientTests>();
+        }
 
-//        [Fact]
-//        public async Task PingPongTest()
-//        {
-//            await server.Start(nameof(PingPongTest));
-//            AdminPortClient client = new AdminPortClient(AdminPortClientSettings.Default, server.ServerInfo, loggerFactory.CreateLogger<AdminPortClient>());
+        [Fact]
+        public async Task PingPongTest()
+        {
+            await server.Start(nameof(PingPongTest));
+            AdminPongEvent pongEvent = null;
+            AdminPortClient client = new AdminPortClient(AdminPortClientSettings.Default, server.ServerInfo, builder =>
+            {
+                builder.AddProvider(new DebugLoggerProvider());
+                builder.AddProvider(new XUnitLoggerProvider(output));
+            });
 
-//            AdminPongEvent pongEvent = null;
-//            client.EventReceived += (_, e) =>
-//            {
-//                if (e is AdminPongEvent pe)
-//                    pongEvent = pe;
-//            };
-
-//            await client.Connect();
-//            client.SendMessage(new AdminPingMessage(55u));
-
-//            while (pongEvent == null)
-//            {
-//                await Task.Delay(1);
-//            }
-
-//            Assert.Equal(55u, pongEvent.PongValue);
-//            await client.Disconnect();
-//        }
-
-//        [Fact]
-//        public async Task AfterServerRestart_AdminPortClientShouldAutomaticallyReconnect()
-//        {
-//            var settings = new AdminPortClientSettings()
-//            {
-//                WatchdogInterval = TimeSpan.FromSeconds(5),
-//            };
-//            logger.LogInformation("Starting Openttd server");
-//            await server.Start(nameof(PingPongTest));
-//            logger.LogInformation("Openttd Server started");
-//            AdminPortClient client = new AdminPortClient(settings, server.ServerInfo, new ContextLogger<AdminPortClient>(loggerFactory.CreateLogger<AdminPortClient>(), "Main Test Client"));
-            
-
-//            logger.LogInformation("Starting client connection");
-//            await client.Connect();
-//            logger.LogInformation("Client connected");
-
-//            logger.LogInformation("Starting openttd server stop");
-//            bool erroredOut = false;
-//            client.StateChanged += (_, arg) => erroredOut = erroredOut | arg.New == AdminConnectionState.Errored;
-//            await server.Stop();
-//            logger.LogInformation("openttd stopped");
-
-//            logger.LogInformation("Waiting for errored state");
+            client.SetAdminEventHandler(ev =>
+            {
+                if (ev is AdminPongEvent pe)
+                {
+                    pongEvent = pe;
+                }
+            });
 
 
-//            if (!(await TaskHelper.WaitUntil(() => erroredOut, delayBetweenChecks: TimeSpan.FromSeconds(0.5), duration: 4 * settings.WatchdogInterval)))
-//            {
-//                throw new AdminPortException("Wrong State!");
-//            }
-//            logger.LogInformation("Errored state established");
+            await client.Connect();
+            client.SendMessage(new AdminPingMessage(55u));
 
-//            await server.Start(nameof(PingPongTest));
+            while (pongEvent == null)
+            {
+                await Task.Delay(1);
+            }
 
-//            logger.LogInformation("Openttd server started (again)");
+            Assert.Equal(55u, pongEvent.PongValue);
+            await client.Disconnect();
+        }
+
+        //[Fact]
+        //public async Task AfterServerRestart_AdminPortClientShouldAutomaticallyReconnect()
+        //{
+        //    var settings = new AdminPortClientSettings()
+        //    {
+        //        WatchdogInterval = TimeSpan.FromSeconds(5),
+        //    };
+        //    logger.LogInformation("Starting Openttd server");
+        //    await server.Start(nameof(PingPongTest));
+        //    logger.LogInformation("Openttd Server started");
+        //    AdminPortClient client = new AdminPortClient(settings, server.ServerInfo, new ContextLogger<AdminPortClient>(loggerFactory.CreateLogger<AdminPortClient>(), "Main Test Client"));
 
 
-//            if (!(await TaskHelper.WaitUntil(() => client.ConnectionState == AdminConnectionState.Connected, delayBetweenChecks: TimeSpan.FromSeconds(0.5), duration: TimeSpan.FromSeconds(20))))
-//            {
-//                throw new AdminPortException("Wrong State!");
-//            }
+        //    logger.LogInformation("Starting client connection");
+        //    await client.Connect();
+        //    logger.LogInformation("Client connected");
 
-//            logger.LogInformation("Connected state established");
+        //    logger.LogInformation("Starting openttd server stop");
+        //    bool erroredOut = false;
+        //    client.StateChanged += (_, arg) => erroredOut = erroredOut | arg.New == AdminConnectionState.Errored;
+        //    await server.Stop();
+        //    logger.LogInformation("openttd stopped");
 
-//        }
+        //    logger.LogInformation("Waiting for errored state");
 
-//        protected virtual void Dispose(bool disposing)
-//        {
-//            if (!disposedValue)
-//            {
-//                if (disposing)
-//                {
-//                    server.Dispose();
-//                    // TODO: dispose managed state (managed objects)
-//                }
 
-//                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-//                // TODO: set large fields to null
-//                disposedValue = true;
-//            }
-//        }
+        //    if (!(await TaskHelper.WaitUntil(() => erroredOut, delayBetweenChecks: TimeSpan.FromSeconds(0.5), duration: 4 * settings.WatchdogInterval)))
+        //    {
+        //        throw new AdminPortException("Wrong State!");
+        //    }
+        //    logger.LogInformation("Errored state established");
 
-//        public void Dispose()
-//        {
-//            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-//            Dispose(disposing: true);
-//            GC.SuppressFinalize(this);
-//        }
-//    }
-//}
+        //    await server.Start(nameof(PingPongTest));
+
+        //    logger.LogInformation("Openttd server started (again)");
+
+
+        //    if (!(await TaskHelper.WaitUntil(() => client.ConnectionState == AdminConnectionState.Connected, delayBetweenChecks: TimeSpan.FromSeconds(0.5), duration: TimeSpan.FromSeconds(20))))
+        //    {
+        //        throw new AdminPortException("Wrong State!");
+        //    }
+
+        //    logger.LogInformation("Connected state established");
+
+        //}
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    server.Dispose();
+                    // TODO: dispose managed state (managed objects)
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
