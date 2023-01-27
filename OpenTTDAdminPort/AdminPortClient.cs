59d5cda9d11c9b29c092e2eda189068cca72be9a
Damian Laczak <shoter@users.noreply.github.com>
2023-01-27 20:29:08 +0100

    chore: more traces

--- OpenTTDAdminPort/AdminPortClient.cs
+++ OpenTTDAdminPort/AdminPortClient.cs
@@ -67,6 +67,7 @@ namespace OpenTTDAdminPort
 
         public async Task Connect()
         {
+            logger.LogTrace($"Asking MainActor {mainActor} to connect to server");
             await mainActor.TryAsk(new AdminPortConnect(ServerInfo, "AdminPortClient"));
         }
 
