b6fb92fcf0c43d701d0b5e141987431a6ba2bb32
Damian Laczak <shoter@users.noreply.github.com>
2023-02-21 02:26:56 +0100

    fix: not complete fix in previous commit

--- OpenTTDAdminPort/IAdminPortClient.cs
+++ OpenTTDAdminPort/IAdminPortClient.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using OpenTTDAdminPort.Events;
+using OpenTTDAdminPort.Game;
 using OpenTTDAdminPort.Messages;
 
 namespace OpenTTDAdminPort
