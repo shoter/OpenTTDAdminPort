f38337a158f1d8cdc694a52fce2a89b0728956b1
Damian Laczak <shoter@users.noreply.github.com>
2023-06-06 19:58:16 +0200

    feat: IncomingMessageProcessor

--- OpenTTDAdminPort/MainActor/IncomingMessageProcessor.cs
+++ OpenTTDAdminPort/MainActor/IncomingMessageProcessor.cs
@@ -1,5 +1,11 @@
 using System;
 using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
+using OpenTTDAdminPort.Assemblies;
+using OpenTTDAdminPort.Common.Assemblies;
+using OpenTTDAdminPort.Events;
+using OpenTTDAdminPort.Events.Creators;
 using OpenTTDAdminPort.MainActor.SingleMessageProcessor;
 using OpenTTDAdminPort.MainActor.StateData;
 using OpenTTDAdminPort.Messages;
@@ -8,13 +14,29 @@ namespace OpenTTDAdminPort.MainActor
 {
     public class IncomingMessageProcessor : IIncomingMessageProcessor
     {
-        private ConcurrentDictionary<Type, ISingleMessageProcessor<>>
+        private ConcurrentDictionary<Type, ISingleMessageProcessor> processors = new();
+
+        public IncomingMessageProcessor()
+        {
+            Assembly assembly = typeof(ISingleMessageProcessor).Assembly;
+
+            var processorTypes = new AssemblyTypeFinder(assembly, GetType().Namespace!)
+                .WithTypeMatcher(new ClassTypeMatcher())
+                .WithTypeMatcher(new ImplementsTypeMatcher(typeof(ISingleMessageProcessor)))
+                .Find();
+
+            foreach(var processorType in processorTypes)
+            {
+                var instance = Activator.CreateInstance(processorType) as ISingleMessageProcessor;
+                processors[processorType] = instance!;
+            }
+        }
 
         public ConnectedData ProcessAdminMessage(
             ConnectedData initial,
-            IAdminMessage message)
-        {
-            throw new System.NotImplementedException();
-        }
+            IAdminMessage message) => processors[message.GetType()]
+            .ProcessAdminMessage(
+                initial,
+                message);
     }
 }
