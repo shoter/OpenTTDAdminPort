1c4e67d418957cb5b289e3314515299cc7f4cd5a
Damian Laczak <shoter@users.noreply.github.com>
2023-10-28 16:55:34 +0200

    feat: more records

--- OpenTTDAdminPort/Messages/AdminChatMessage.cs
+++ OpenTTDAdminPort/Messages/AdminChatMessage.cs
@@ -2,26 +2,15 @@
 
 namespace OpenTTDAdminPort.Messages
 {
-    public class AdminChatMessage : IAdminMessage
+    public record AdminChatMessage(
+        NetworkAction NetworkAction,
+        ChatDestination ChatDestination,
+        uint Destination,
+        string Message) : IAdminMessage
     {
         public AdminMessageType MessageType => AdminMessageType.ADMIN_PACKET_ADMIN_CHAT;
 
-        public NetworkAction NetworkAction { get; }
-
-        public ChatDestination ChatDestination { get; }
-
-        public uint Destination { get; }
-
-        public string Message { get; }
-
-        public AdminChatMessage(NetworkAction networkAction, ChatDestination chatDestination, uint destination, string message)
-        {
-            this.NetworkAction = networkAction;
-            this.ChatDestination = chatDestination;
-            this.Message = message;
-            this.Destination = destination;
-        }
-
-        public override string ToString() => $"{MessageType} - {NetworkAction} - {ChatDestination} - {Destination} - {Message}";
+        public override string ToString() =>
+            $"{MessageType} - {NetworkAction} - {ChatDestination} - {Destination} - {Message}";
     }
 }
