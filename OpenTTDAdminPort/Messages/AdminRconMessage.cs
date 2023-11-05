889f66c9dd855899670d428ee8057c1d468f0949
Damian <3819892+shoter@users.noreply.github.com>
2023-11-05 14:13:57 +0100

    feat: rcon as record (#24)
    * feat: more records
    
    * fix: Build Error
    
    * feat: Rcon msg as record
    
    ---------
    
    Co-authored-by: Damian Laczak <shoter@users.noreply.github.com>

--- OpenTTDAdminPort/Messages/AdminRconMessage.cs
+++ OpenTTDAdminPort/Messages/AdminRconMessage.cs
@@ -6,15 +6,8 @@ using System.Threading.Tasks;
 
 namespace OpenTTDAdminPort.Messages
 {
-    public class AdminRconMessage : IAdminMessage
+    public record AdminRconMessage(string Command) : IAdminMessage
     {
         public AdminMessageType MessageType => AdminMessageType.ADMIN_PACKET_ADMIN_RCON;
-
-        public string Command { get; }
-
-        public AdminRconMessage(string command)
-        {
-            this.Command = command;
-        }
     }
 }
